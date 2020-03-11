dc0c597f36f680980298e9e3c1b5229ca6d5d874
Trifosa <misael.trifosa@gmail.com>
2020-03-11 14:58:07 +0700

    second

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-sk
+const DB_USER = [
+	{
+		username: "tamamo",
+		password: "password"
+	}
+]
+
+const inputUsername = document.querySelector("#username");
+const inputPassword = document.querySelector("#password");
+const buttonSubmit = document.querySelector("#submitForm");
+
+buttonSubmit.addEventListener('click', (e) => {
+	e.preventDefault();
+	const valueInputPassword = inputPassword.value;
+	const valueInputUsername = inputUsername.value;
+	console.log(valueInputUsername);
+	console.log(valueInputPassword);
+	
+	let flag = 0;
+	DB_USER.map((data) =>{
+		if(data.username === valueInputUsername && data.password === valueInputPassword){
+			flag = 1;
+		}
+	})
+	
+	const warningMsg = document.querySelector(".warning");
+	
+	if(flag === 0){
+		warningMsg.className = "text-danger"
+		//console.log("Password atau Username Salah!");
+	}else{
+		alert("Selamat anda masuk ke home");
+	}
+})
